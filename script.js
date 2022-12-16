429b76e55ba9560b2594efe45f024d1295115b76
Jaime Luong <jaime.luong@gmail.com>
2022-12-15 21:45:06 -0600

    Add basic sketch functionality

--- script.js
+++ script.js
@@ -4,7 +4,15 @@ const container = document.querySelector('.container');
 // Generate 256 squares to start (16x16 grid)
 for(var i=0; i<256; i++) {
     const square = document.createElement('div'); // Create a basic square div
-    square.setAttribute('style', 'background-color: gray; height: 50px; width: 50px;');
+    square.setAttribute('style', 'height: 50px; width: 50px;');
     square.classList.add('square');
     container.appendChild(square);
 }
+
+// Get a nodelist of all the squares in order to add event listeners on each
+const squares = document.querySelectorAll('.square')
+
+// Add event listener to each square to turn black
+squares.forEach(square => square.addEventListener('mouseover', () => {
+    square.classList.add('colored');
+}));
