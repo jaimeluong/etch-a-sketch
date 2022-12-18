7db55f5b293844a52fccfefd9df0a7cc66237046
Jaime Luong <jaime.luong@gmail.com>
2022-12-18 14:59:20 -0600

    Misc refactor and clean-up

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Etch-a-Sketch
 
-This is a browser-based game of Etch-a-Sketch where you can mouse over squares to color them in. It's built using a combination of HTML, CSS, and JavaScript.
+This is a browser-based game of Etch-a-Sketch where you can mouse over squares to color them in. You can change the grid size, clear the board, and it paints in randomly-generated colors. It's built using a combination of HTML, CSS, and JavaScript.
 
-To do:
+To do... eventually:
 - Add feature to darken square with each pass
--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 // Get container element to put squares inside
 const container = document.querySelector('.container');
 
+// Global variable for squares per side
 let squaresPerSide;
 
 // Set number of squares per side
-function askForSquares() { // Function definition
+function askForSquares() {
     squaresPerSide = prompt("How many squares per side?");
     generateSquares(squaresPerSide);
 }
 
 // Generate squares in memory and place in container
-function generateSquares(num) { // Function definition
+function generateSquares(num) {
     for(var i=0; i<num*num; i++) {
         const square = document.createElement('div');
         square.setAttribute('style', `height: ${800/num}px; width: ${800/num}px;`);
@@ -19,11 +20,13 @@ function generateSquares(num) { // Function definition
     }
 }
 
-askForSquares(); // Upon page load, run askForSquares() once
+// Upon page load, run askForSquares() once
+askForSquares();
 
+// Global variable for nodelist of squares
 let squares = document.querySelectorAll('.square'); // Get all square divs
 
-// Add event listeners for all squares
+// Add mouseover event listeners to all squares
 function addEventListeners() {
     squares.forEach(square => square.addEventListener('mouseover', () => {
         var randomColor = Math.floor(Math.random()*16777215).toString(16); // Generate a random hexadecimal-formatted color
@@ -31,17 +34,19 @@ function addEventListeners() {
     }));
 }
 
+// Add event listeners
 addEventListeners();
 
-// Clear board when clicked
+// Clear board when requested
 const clearButton = document.querySelector('#clear');
 clearButton.addEventListener('click', () => {
     squares.forEach(square => square.style.backgroundColor = 'white'); // Set all squares to white
 });
 
+// Prompt user to change grid size
 const editButton = document.querySelector('#edit');
 editButton.addEventListener('click', () => {
-    squares.forEach(square => square.remove()); // Remove each div in nodelist
+    squares.forEach(square => square.remove()); // Remove each square div in nodelist
     askForSquares(); // Prompt user for number of squares again
     squares = document.querySelectorAll('.square'); // Reassign squares nodelist
     addEventListeners(); // Add new event listeners
